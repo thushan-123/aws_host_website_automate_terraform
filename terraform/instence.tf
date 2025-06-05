8e455170c7a7c7ec1ab6b28311efd401bbebf205
thushan-123 <thushanmadusanka456@gmail.com>
2025-06-05 16:45:15 +0530

    ec2 add key-pair

--- terraform/instence.tf
+++ terraform/instence.tf
@@ -4,6 +4,10 @@ resource "aws_instance" "web" {
   subnet_id = aws_subnet.it_department_subnet.id
   vpc_security_group_ids = [aws_security_group.web_security_group.id]
 
+  key_name = var.key_name
+
+  user_data = local.ec2_user_data
+
   tags = {
     Name = "Voldemort HTTP WEB SERVER"
   }

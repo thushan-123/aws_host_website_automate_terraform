fdeefe34a56c70d2af581f01d3dd56b8e9fde5c7
thushan-123 <thushanmadusanka456@gmail.com>
2025-06-05 18:54:50 +0530

    attach to key pair

--- terraform/instence.tf
+++ terraform/instence.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
   subnet_id = aws_subnet.it_department_subnet.id
   vpc_security_group_ids = [aws_security_group.web_security_group.id]
 
-  key_name = var.key_name
+  key_name = aws_key_pair.ec2_key.key_name
 
   user_data = local.ec2_user_data
 

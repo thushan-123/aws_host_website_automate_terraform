ce4ce2e9c8d0da621194b8f961753882aa1a29ec
thushan-123 <thushanmadusanka456@gmail.com>
2025-06-05 16:24:40 +0530

    add to ami id

--- terraform/instence.tf
+++ terraform/instence.tf
@@ -1,6 +1,10 @@
 resource "aws_instance" "web" {
-  ami = ""
+  ami = var.ami_id
   instance_type = "t2.micro"
   subnet_id = aws_subnet.it_department_subnet.id
   vpc_security_group_ids = [aws_security_group.web_security_group.id]
+
+  tags = {
+    Name = "Voldemort HTTP WEB SERVER"
+  }
 }
